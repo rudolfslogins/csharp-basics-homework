7d9eebd46845110d20e3876041b7a58dc1d2b154
Rudolfs Logins <rudolfs.logins@gmail.com>
2019-08-19 19:36:26 +0300

    Adding loops homework

---
+++ loops/Exercise6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 'FizzBuzz' number below:");
+            int max = Convert.ToInt32(Console.ReadLine());
+
+            for (var i = 2; i <= max; i++)
+            {
+                bool flag = true;
+                if (i % 3 == 0)
+                {
+                    Console.Write("Fizz");
+                    flag = false;
+                }
+                if (i % 5 == 0)
+                {
+                    Console.Write("Buzz");
+                    flag = false;
+                }
+                if (flag)
+                {
+                    Console.Write(i);
+                }
+
+                Console.Write(" ");
+
+                if (i % 20 == 0)
+                {
+                    Console.WriteLine("");
+                }
+            }
+            Console.ReadKey();
+        
+    }
+    }
+}
