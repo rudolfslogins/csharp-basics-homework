f5da727b1755ac9418c8598864e57884b21cfc33
Rudolfs Logins <rudolfs.logins@gmail.com>
2019-08-19 19:41:54 +0300

    Adding Arithmetics homework

---
+++ Arithmetic/Exercise6/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 'CozaLozaWoza' number below:");
+            int result = Convert.ToInt32(Console.ReadLine());
+
+            for (var i = 1; i <= result; i++)
+            {
+                bool flag = true;
+                if (i % 3 == 0)
+                {
+                    Console.Write("Coza");
+                    flag = false;
+                } 
+                if (i % 5 == 0)
+                {
+                    Console.Write("Loza");
+                    flag = false;
+                }
+                if (i % 7 == 0)
+                {
+                    Console.Write("Woza");
+                    flag = false;
+                }
+                if (flag)
+                {
+                    Console.Write(i);
+                }
+
+                Console.Write(" ");
+
+                if (i % 11 == 0)
+                {
+                    Console.WriteLine("");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Arithmetic/GravityCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GravityCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double gravity = -9.81;  // Earth's gravity in m/s^2
+            double initialVelocity = 0.0;
+            double fallingTime = 10.0;
+            double initialPosition = 0.0;
+            double finalPosition = GetFinalPosition(gravity, fallingTime, initialVelocity, initialPosition);
+            //double finalPosition = 0.5 * ((gravity * fallingTime)* (gravity * fallingTime))
+            Console.WriteLine("The object's position after " + fallingTime + " seconds is " + finalPosition + " m.");
+            Console.ReadKey();
+        }
+        static double GetFinalPosition (double a, double t, double v, double x)
+        {
+            return (0.5 * (a * (t * t))) + (v * t) + x;
+        }
+    }
+}
