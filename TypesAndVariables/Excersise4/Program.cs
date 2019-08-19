fa404121cc4a4b9b11809beb60f18cfeba9d9b21
Rudolfs Logins <rudolfs.logins@gmail.com>
2019-08-19 23:48:58 +0300

    Adding TypesAndVariables

---
+++ TypesAndVariables/Excersise4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Juan Valdez";
+            int birthYear = 1995;
+
+            Console.WriteLine($"My name is { name } and I was born in { birthYear }.");
+            Console.ReadKey();
+        }
+    }
+}
