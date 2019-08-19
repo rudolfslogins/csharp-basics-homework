014ea72a4e40dea367d6c48e449ce6ba07f6bdb4
Rudolfs Logins <rudolfs.logins@gmail.com>
2019-08-19 19:52:44 +0300

    Added TypesAndVariables

---
+++ TypesAndVariables/Exercise7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] upperList =
+            {
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'V',
+                'Z', 'X', 'Y', 'W', 'Q'
+            };
+            int uppercaseCount = 0;
+            Console.WriteLine("Please enter a string below:");
+            String input = Console.ReadLine();
+            char[] inputToCharArray = input.ToCharArray();
+            for (var i = 0; i < inputToCharArray.Length; i++)
+            {
+                for (var n = 0; n < upperList.Length; n++)
+                {
+                    if (inputToCharArray[i] == upperList[n])
+                    {
+                        uppercaseCount++;
+                    }
+                }
+            }
+            Console.WriteLine("Uppercase letter count: " + uppercaseCount);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TypesAndVariables/Exercise9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input distance in meters: ");
+            double distanceInMeters = Convert.ToDouble(Console.ReadLine()); 
+            Console.WriteLine("Input hour: ");
+            double hoursTaken = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Input minutes: ");
+            double minutesTaken = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Input seconds: ");
+            double secondsTaken = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Your speed in meters/second is: " + Math.Round(distanceInMeters / (secondsTaken + (minutesTaken * 60) + (hoursTaken * 3600)), 8));
+            Console.WriteLine("Your speed in km/h is: " + Math.Round(((float) distanceInMeters / 1000) / ( (float) hoursTaken + ((float)minutesTaken / 60) + ((float)secondsTaken / 3600)), 8));
+            Console.WriteLine("Your speed in miles/h is: " + Math.Round((distanceInMeters / 1609) / (hoursTaken + (minutesTaken / 60) + (secondsTaken / 3600)), 8));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
