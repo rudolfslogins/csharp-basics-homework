22654f2a373b01cd969e77940c397900290d50b6
Rudolfs Logins <rudolfs.logins@gmail.com>
2019-08-19 23:16:26 +0300

    Adding Loops

---
+++ Loops/Exercise1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            Console.WriteLine("The first 10 natural numbers are: ");
+
+            /*
+            todo - write a program in C# to display the first 10 natural numbers
+            */
+            for (i = 1; i <= 10; i++) {
+              Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Loops/Exercise4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Exercise4
+{
+    class Program
+    {
+        //TODO: print all vowels using for and foreach
+        static void Main(string[] args)
+        {
+            char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+
+            /*
+            todo - use for*/
+            for (var i = 0; i < vowels.Length; i++) {
+                Console.WriteLine(vowels[i]);
+            }
+
+            Console.ReadKey();
+
+            /*
+            todo - use foreach*/
+            foreach (var VARIABLE in vowels)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
