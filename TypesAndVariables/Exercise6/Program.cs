be841245996feac594bd50d107f7516a26143759
Rudolfs Logins <rudolfs.logins@gmail.com>
2019-08-22 19:02:53 +0300

    Adding source files

---
+++ TypesAndVariables/Exercise6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter digits below:");
+            var input = Console.ReadLine();
+            char[] charInput = input.ToCharArray();
+            int arraySum = 0;
+            for (var i = 0; i < charInput.Length; i++)
+            {
+                arraySum = arraySum + Convert.ToInt16(charInput[i]) - 48;
+            }
+            Console.WriteLine(arraySum);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ TypesAndVariables/Exercise8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var minutesInYear = 60 * 24 * 365;
+            Console.WriteLine("Please enter number of minutes below: ");
+            String input = Console.ReadLine();
+
+            var years = Convert.ToInt32(input) / minutesInYear;
+            var days = (Convert.ToInt32(input) % minutesInYear) / 60 / 24;
+
+            Console.WriteLine($"{ input } minutes is { years } years and { days } days!");
+            Console.ReadKey();
+        }
+    }
+}
